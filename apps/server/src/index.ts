8a480543648bf2e2c928d8f2b25294f378e63491
Ajai-3 <ajaipjayan@gmail.com>
2025-10-16 17:17:28 +0530

    feat: add dotenv and ioredis dependencies for environment configuration and Redis integration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,18 +2,18 @@ import http from "http";
 import SocketService from "./services/socket";
 
 async function init() {
-    const socketService = new SocketService();
+  const socketService = new SocketService();
 
-    const httpServer = http.createServer();
-    const PORT = process.env.PORT ? process.env.PORT : 8000;
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT ? process.env.PORT : 8000;
 
-    socketService.io.attach(httpServer)
+  socketService.io.attach(httpServer);
 
-    httpServer.listen(PORT, () => {
-        console.log(`HTTP Server running on port ${PORT} 🐛`)
-    })
+  socketService.initListners();
 
-    socketService.initListners()
+  httpServer.listen(PORT, () => {
+    console.log(`HTTP Server running on port ${PORT} 🐛`);
+  });
 }
 
-init()
+init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,22 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config()
+import Redis from "ioredis";
 import { Server } from "socket.io";
+
+const pub = new Redis(process.env.REDIS_URL || "");
+const sub = new Redis(process.env.REDIS_URL || "");
 
 class SocketService {
   private _io: Server;
   constructor() {
     console.log("Init Socket Service 📡");
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        origin: "*",
+        allowedHeaders: ["*"],
+      },
+    });
+
+    sub.subscribe("MESSEGES")
   }
 
   public initListners() {
     const io = this.io;
-    console.log("Init Socket Listners....")
+    console.log("Init Socket Listners....");
     io.on("connect", (socket) => {
-      console.log(`New Socket Connected 🛜`, socket.id);
+      console.log(`New Socket Connected ${socket.id} 🛜`);
 
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("New Message recived 💬", message);
+
+        // Publish the message
+        await pub.publish('MESSEGES', JSON.stringify({ message }))
       });
     });
+
+    sub.on('message', (channel, message) => {
+      if (channel === 'MESSEGES') {
+        io.emit("message", message)
+      }
+    })
+
   }
 
   get io() {
